84e1455047565c66c1ead4062ab36afaa8838035
mayakiyomi <mayakiyomi@gmail.com>
2026-02-19 12:10:32 -0800

    everypic v2

--- everypicture/v2/script.js
+++ everypicture/v2/script.js
@@ -1,9 +1,7 @@
 (function () {
 	'use strict';
 
-    console.log('reading js');
 	
-
 	window.onbeforeunload = function () {
 		window.scrollTo(0, 0);
 	}
@@ -15,30 +13,47 @@
 		let counter = 1;
 		let prevCounter = 1;
 		let doneResizing;
-		let exitDirection;
-		let enterDirection;
 
-		/* This version adds a preloader screen that shows until all
-		the assets for the page have downloaded, including the large
-		image. This preloader is a div that covers the entire screen 
-		on the HTML file.
-		
-		The code below removes this div by fading it out, then once
-		it has faded out, sets it to display none. */
-
-		const preloader = document.getElementById('preloader');
+		const preloader = document.querySelector('#preloader');
 		preloader.className = 'fadeout';
 
-		// wait until the animation has completed
+		
 		preloader.addEventListener('animationend', function () {
 
-			//once the animation is done, remove the preloader div.
+		
 			preloader.style.display = 'none';
 		});
 
 
 		resetPagePosition();
 
+		window.addEventListener('scroll', function () {
+			pageTop = window.pageYOffset + 300;
+
+			if (pageTop > postTops[counter]) {
+				counter++;
+				console.log(`scrolling down ${counter}`);
+			} else if (counter > 1 && pageTop < postTops[counter - 1]) {
+				counter--;
+				console.log(`scrolling up ${counter}`);
+			}
+
+			if (counter != prevCounter) {
+				document.querySelector('figure img').className = 'sect' + counter;
+
+
+				prevCounter = counter;
+			}
+
+		}); 
+		window.addEventListener('resize', function () {
+			clearTimeout(doneResizing);
+			doneResizing = setTimeout(function () {
+
+				resetPagePosition();
+
+			}, 500);
+		});
 
 		function resetPagePosition() {
 			postTops = [];
@@ -50,9 +65,7 @@
 			counter = 0;
 
 			postTops.forEach(function (post) { if (pagePosition > post) { counter++; } });
-
 		}
+	});
 
-	}); // end window load function
-
-})();// END IIFE
+})();
