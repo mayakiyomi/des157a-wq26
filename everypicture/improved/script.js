9d97c422e2ae5a43b9b4e5ed27defe31c8758290
mayakiyomi <mayakiyomi@gmail.com>
2026-03-04 17:53:12 -0800

    improvements:button to scroll

---
+++ everypicture/improved/script.js
@@ -0,0 +1,79 @@
+(function () {
+	'use strict';
+	console.log('reading js');
+	
+	window.onbeforeunload = function () {
+		window.scrollTo(0, 0);
+	}
+
+	window.addEventListener('load', function () {
+		const posts = document.querySelectorAll('section');
+		const btn = this.document.querySelector('#reset');
+		let postTops = [];
+		let pageTop;
+		let counter = 1;
+		let prevCounter = 1;
+		let doneResizing;
+
+		const preloader = document.querySelector('#preloader');
+		preloader.className = 'fadeout';
+
+		
+		preloader.addEventListener('animationend', function () {
+
+		
+			preloader.style.display = 'none';
+		});
+
+
+		resetPagePosition();
+
+		window.addEventListener('scroll', function () {
+			pageTop = window.pageYOffset + 300;
+
+			if (pageTop > postTops[counter]) {
+				counter++;
+				console.log(`scrolling down ${counter}`);
+			} else if (counter > 1 && pageTop < postTops[counter - 1]) {
+				counter--;
+				console.log(`scrolling up ${counter}`);
+			}
+
+			if (counter != prevCounter) {
+				document.querySelector('figure img').className = 'sect' + counter;
+
+
+				prevCounter = counter;
+			}
+
+		}); 
+		window.addEventListener('resize', function () {
+			clearTimeout(doneResizing);
+			doneResizing = setTimeout(function () {
+
+				resetPagePosition();
+
+			}, 500);
+		});
+
+		function resetPagePosition() {
+			postTops = [];
+			posts.forEach(function (post) {
+				postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
+			});
+
+			const pagePosition = window.pageYOffset + 300;
+			counter = 0;
+
+			postTops.forEach(function (post) { if (pagePosition > post) { counter++; } });
+		}
+
+		btn.addEventListener('click', function(){
+			window.scrollTo({
+				top : 0,
+				behavior : 'smooth'
+			})
+		})
+	});
+
+})();
